f1a54e3039d97090fcb3b116a9574864f6dea5c5
Lucifer2op <87086143+Lucifer2op@users.noreply.github.com>
2021-07-07 22:08:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,28 +8,28 @@ import os
 TOKEN = os.environ.get("TOKEN", "")
 API_ID = int(os.environ.get("API_ID",12345))
 API_HASH = os.environ.get("API_HASH","")
-BITLY_TOKEN = os.environ.get("BITLY_TOKEN","")
+ADRINI_TOKEN = os.environ.get("ADRINO_TOKEN","")
 
 headers = {
-    'Authorization': BITLY_TOKEN,
+    'Authorization': ADRINO_TOKEN,
     'Content-Type': 'application/json',
 }
 
 
-app = Client("bitlybot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
+app = Client("pdiskbot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client,message):
-  await message.reply_text(f"Hello {message .from_user.first_name}\nhello i am bit.ly short link genrator\n made with love by @mrlokaman ", reply_to_message_id = message.message_id)
+  await message.reply_text(f"Hello {message .from_user.first_name}\nhello i am adrinolinks.in short link genrator\n made with love by @Lucifer_morning_star_op ", reply_to_message_id = message.message_id)
   
 @app.on_message(filters.private & filters.regex("http|https"))
-async def Bitly(client,message):
+async def Adrino(client,message):
   URL = message.text
-  DOMAIN = "bit.ly"
+  DOMAIN = "adrinolinks.in"
   value  = {'long_url': URL , 'domain': DOMAIN}
   data = json.dumps(value)
   try:
-    r = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers,data = data )
+    r = requests.post('https://adrinolinks.in/api?api=4b46eb8081b4c2e0cfeffb4d6b479ab5b627e0f6&url=yourdestinationlink.com&alias=CustomAlias', headers=headers,data = data )
     result = r.json()
     link = result["link"]
     await message.reply_text(f"```{link}```", reply_to_message_id= message.message_id)
