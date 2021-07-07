f4cda8cded86344d7129d37d8e0ddc99deaad540
Lucifer2op <87086143+Lucifer2op@users.noreply.github.com>
2021-07-08 00:05:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,12 +29,15 @@ async def Adrino(client,message):
   value  = {'long_url': URL , 'domain': DOMAIN}
   data = json.dumps(value)
   try:
-r = requests.post('api_url = "https://adrinolinks.in/api?api={$api_id}&url=', headers=headers,data = data )
-    result = r.json()
-    link = result["link"]
-    await message.reply_text(f"```{link}```", reply_to_message_id= message.message_id)
-  except Exception as e :
-    await message.reply_text(e)
+$long_url = urlencode('yourdestinationlink.com');
+$api_id = '4b46eb8081b4c2e0cfeffb4d6b479ab5b627e0f6';
+$api_url = "https://adrinolinks.in/api?api={$api_id}&url={$long_url}&alias=CustomAlias";
+$result = @json_decode(file_get_contents($api_url),TRUE);
+if($result["status"] === 'error') {
+ echo $result["message"];
+} else {
+ echo $result["shortenedUrl"];
+}
     
   
 app.run()
