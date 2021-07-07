b3a6601d300514500a6481af1c2465a6bc4cfc4b
Lucifer2op <87086143+Lucifer2op@users.noreply.github.com>
2021-07-07 23:00:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def Adrino(client,message):
   data = json.dumps(value)
   try:
     r = requests.post('https://adrinolinks.in/api?api={$api_token}&url={$long_url}&alias=CustomAlias', headers=headers,data = data )
-    result = r.json()
+    result = r.json(file_get_contents($api_url),TRUE)
     link = result["link"]
     await message.reply_text(f"```{link}```", reply_to_message_id= message.message_id)
   except Exception as e :
