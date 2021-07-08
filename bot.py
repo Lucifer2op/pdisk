fd9b55153a2656e7b15675603f27961c8453e91c
Lucifer2op <87086143+Lucifer2op@users.noreply.github.com>
2021-07-08 12:32:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ app = Client("pdiskbot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client,message):
   await message.reply_text(f"Hello {message .from_user.first_name}\nhello i am adrinolinks.in short link genrator\n made with love by @Lucifer_morning_star_op ", reply_to_message_id = message.message_id)
- cURL = message.text
+  URL = message.text
   DOMAIN = "https://adrinolinks.in"
   value  = {'long_url': URL , 'domain': DOMAIN}
   data = json.dumps(value)
